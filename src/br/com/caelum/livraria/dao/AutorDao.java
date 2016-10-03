723693411b5cec4cb33e43f6378a4df38c10e810
Ramon Vieira <ramonsv89@gmail.com>
2016-10-02 22:22:47 -0300

    Aula 6 - Novos serviços com Interceptadores

--- src/br/com/caelum/livraria/dao/AutorDao.java
+++ src/br/com/caelum/livraria/dao/AutorDao.java
@@ -8,9 +8,11 @@ import javax.ejb.TransactionAttribute;
 import javax.ejb.TransactionAttributeType;
 import javax.ejb.TransactionManagement;
 import javax.ejb.TransactionManagementType;
+import javax.interceptor.Interceptors;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import br.com.caelum.livraria.interceptador.LogInterceptador;
 import br.com.caelum.livraria.modelo.Autor;
 
 /**
@@ -20,6 +22,7 @@ import br.com.caelum.livraria.modelo.Autor;
 
 @Stateless
 @TransactionManagement(TransactionManagementType.CONTAINER)
+//@Interceptors({LogInterceptador.class})
 public class AutorDao {
 
 	@PersistenceContext
